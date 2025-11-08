0fd015335913914f879b6a07b03e6e153a5266a2
crisjohn0627 <crisjohn.canales@msunaawan.edu.ph>
2025-11-08 22:20:40 +0800

    List properties on the front page (static)- Cris John Canales

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import PropertyList from "./components/properties/PropertyList";
 import Categories from "./components/Categories";
 export default function Home() {
   return (
       <main className="">
         <Categories />
+
+        <div className="mt-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
+            <PropertyList />
+        </div>
       </main>
   );
 }
