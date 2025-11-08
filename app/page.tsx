2563a10243fb59df93bad24637baa67135ae7bb6
crisjohn0627 <crisjohn.canales@msunaawan.edu.ph>
2025-11-08 22:05:37 +0800

    Categories on the front page (static)- Cris John Canales

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
-
+import Categories from "./components/Categories";
 export default function Home() {
   return (
       <main className="">
-        DjangoBnb
-
-        <h2 className="text-airbnb">Django and next rules</h2>
+        <Categories />
       </main>
   );
 }
