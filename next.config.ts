a96daeca95914c715a0302cb99c71994f6018487
crisjohn0627 <crisjohn.canales@msunaawan.edu.ph>
2025-11-24 22:37:20 +0800

    Video 3-Get properties in the front end and list them - Cris John M Canales

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/**'
+      },
+    ],
+  },
 };
 
 export default nextConfig;
