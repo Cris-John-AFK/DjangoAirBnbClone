99868576a2e15939014a7efa4a44b20b1dc421bc
crisjohn0627 <crisjohn.canales@msunaawan.edu.ph>
2025-11-25 12:45:03 +0800

    Video 4-Create api service file (And use it in the property list)- Cris John Canales

--- app/components/properties/PropertyList.tsx
+++ app/components/properties/PropertyList.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import PropertyListItem from "./PropertyListItem";
+import apiService from "@/app/services/apiService";
 
 export type PropertyType = {
     id: string;
@@ -13,20 +14,10 @@ export type PropertyType = {
 const PropertyList = () => {
     const [properties, setProperties] = useState<PropertyType[]>([]);
     const getProperties = async () => {
-        const url = 'http://localhost:8000/api/properties/';
 
-        await fetch(url, {
-            method: 'GET',
-        })
-            .then(response => response.json())
-            .then((json) => {
-                console.log('json', json);
+        const tmpProperties = await apiService.get('/api/properties/');
 
-                setProperties(json.data);
-            })
-            .catch((error) => {
-                console.log('error', error);
-            })
+        setProperties(tmpProperties.data);
     }
     useEffect(() => {
         getProperties();
