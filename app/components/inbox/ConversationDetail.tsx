a27de92e2e9df79b0f23e2b4c86b4d0bd6f6c515
crisjohn0627 <crisjohn.canales@msunaawan.edu.ph>
2025-12-11 10:36:55 +0800

    Video 7- Make it possible to connect- Cris John M. Canales

--- app/components/inbox/ConversationDetail.tsx
+++ app/components/inbox/ConversationDetail.tsx
@@ -20,11 +20,11 @@ const ConversationDetail = () => {
             </div>
 
             <div className="mt-4 py-4 px-6 flex border border-gray-300 space-x-4 rounded-xl">
-                <input type="text" 
-                placeholder="Type your message..." 
-                className="w-full p-2 bg-gray-200 rounded-xl" />
+                <input type="text"
+                    placeholder="Type your message..."
+                    className="w-full p-2 bg-gray-200 rounded-xl" />
 
-                <CustomButton 
+                <CustomButton
                     label="Send"
                     onClick={() => console.log("Clicked")}
                     className="w-[100px]"
